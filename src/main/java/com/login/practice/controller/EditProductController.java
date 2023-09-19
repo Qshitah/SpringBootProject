63a3d979869db50c12c8b96a614a8db27d4db1ad
Marouan <marouan.akechtah@gmail.com>
2023-09-19 12:15:42 +0100

    Add Edit Product Page

--- src/main/java/com/login/practice/controller/EditProductController.java
+++ src/main/java/com/login/practice/controller/EditProductController.java
@@ -1,6 +1,5 @@
 package com.login.practice.controller;
 
-import com.dropbox.core.DbxException;
 import com.login.practice.Entity.Category;
 import com.login.practice.Entity.ParentCategory;
 import com.login.practice.Entity.Product;
@@ -8,10 +7,8 @@ import com.login.practice.repository.CategoryRepository;
 import com.login.practice.repository.ParentCategoryRepository;
 import com.login.practice.repository.ProductRepository;
 import com.login.practice.service.ImageService;
-import com.login.practice.signUp.entity.User;
 import com.login.practice.signUp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +16,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
-import java.io.IOException;
-import java.time.LocalDateTime;
+import java.io.File;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
-public class AddProductController {
+public class EditProductController {
 
     @Autowired
     private UserRepository userRepository;
@@ -44,14 +39,22 @@ public class AddProductController {
 
     private final ImageService imageService;
 
-    public AddProductController(ImageService imageService) {
+    public EditProductController(ImageService imageService) {
         this.imageService = imageService;
     }
 
-    @GetMapping("/add-product")
-    public String addProduct(Model model){
-        Product product = new Product();
+    @GetMapping(value = "/edit-product",params = "productId")
+    public String editProduct(@RequestParam("productId") int productId, Model model){
 
+        Optional<Product> productOptional = productRepository.findById(productId);
+        if (productOptional.isEmpty()){
+            return "error-404";
+        }
+        model.addAttribute("product", productOptional.get());
+
+
+        List<String> images = List.of(productOptional.get().getProductImages().split("/"));
+        model.addAttribute("images",images);
 
         List<Category> categories = categoryRepository.findAll();
         model.addAttribute("categories", categories);
@@ -59,31 +62,24 @@ public class AddProductController {
         List<ParentCategory> parentCategories = parentCategoryRepository.findAll();
         model.addAttribute("parentCategories", parentCategories);
 
-        model.addAttribute("product", product);
-
-
-
-        return "add-product";
+        return "edit-product";
     }
 
-    @PostMapping("/add-product")
-    public String postAddProduct(@ModelAttribute("product") Product product,
-                                 @RequestParam("category") String categoryId,
-                                 @RequestParam("parentCategory") String parentCategoryId,
-                                 @RequestParam("image-uploadify") List<MultipartFile> files,
-                                 Authentication authentication,
-                                 Model model)  throws DbxException {
-
-
-        User currentUser = userRepository.findByUsername(authentication.getName());
-
+    @PostMapping(value = "/edit-product",params = "productId")
+    public String saveEditProduct(@ModelAttribute("product") Product product,
+                                  @RequestParam("category") String categoryId,
+                                  @RequestParam("category.parentCategory") String parentCategoryId,
+                                  @RequestParam("image-uploadify") List<MultipartFile> files,
+                                  @RequestParam("combinedImages") String images,
+                                  Model model
+                                  ){
         List<Category> categories = categoryRepository.findAll();
         model.addAttribute("categories", categories);
 
         List<ParentCategory> parentCategories = parentCategoryRepository.findAll();
         model.addAttribute("parentCategories", parentCategories);
-        product.setUser(currentUser);
 
+        Optional<Product> oldProduct = productRepository.findById(product.getId());
 
         String productName = product.getProductName();
         if(productName == null || productName.isEmpty()){
@@ -91,10 +87,11 @@ public class AddProductController {
             return "add-product";
         }
 
-        if(productRepository.findByProductName(productName) != null){
+        if(productRepository.findByProductName(productName) != null && productRepository.findByProductName(productName).getId() != product.getId() ){
             model.addAttribute("existsProductName",true);
             return "add-product";
         }
+
 
         String sku = product.getSku();
         if(sku == null || sku.isEmpty()){
@@ -108,10 +105,6 @@ public class AddProductController {
             return "add-product";
         }
 
-        if (files.get(0).isEmpty()){
-            model.addAttribute("failedFile",true);
-            return "add-product";
-        }
 
         Float price = product.getRegularPrice();
         if(price == null || price == 0){
@@ -151,21 +144,43 @@ public class AddProductController {
         Optional<Category> category = categoryRepository.findById(Integer.valueOf(categoryId));
         category.ifPresent(product::setCategory);
 
-        imageService.uploadImages(files,product);
+        if (oldProduct.isPresent()){
+            if (!Objects.equals(product.getProductName(), oldProduct.get().getProductName())){
+                System.out.println("Ooooh Yeeees");
+                // Define the old and new folder paths
+                String oldFolderPath = "src/main/resources/static/public/images/products/" + oldProduct.get().getProductName();
+                String newFolderPath = "src/main/resources/static/public/images/products/" + product.getProductName();
+
+                // Create File objects for old and new folders
+                File oldFolder = new File(oldFolderPath);
+                System.out.println(oldFolder);
+                File newFolder = new File(newFolderPath);
+                System.out.println(newFolder);
+                // Check if the old folder exists before renaming
+                if (oldFolder.exists()) {
+                    if(oldFolder.renameTo(newFolder)){
+                        System.out.println("Done Renaming Folder");
+                    }
+                }
+            }
+        }
+
+        if (!files.get(0).isEmpty()){
+            imageService.uploadImages(files,product);
+            product.setProductImages(images + product.getProductImages());
+        }else{
+            product.setProductImages(images);
+        }
+
 
         product.setIsActive(1);
 
         productRepository.save(product);
 
-        return "/";
+        List<String> imagesAfterSaving = List.of(product.getProductImages().split("/"));
+        model.addAttribute("images",imagesAfterSaving);
 
-    }
 
-    private String generateUniqueFileName(String originalFileName) {
-        // Implement your logic to generate a unique file name
-        // This can involve adding timestamps, UUIDs, or other strategies
-        // Here's a simple example using a timestamp:
-        long timestamp = System.currentTimeMillis();
-        return timestamp + "_" + originalFileName;
+        return "redirect:/edit-product?productId=" + product.getId();
     }
 }
